5ef00e908d675ab7f96b21dbb6fc23d1a4bc81a6
varshakarthikeyan123 <varshakarthikeyan1@gmail.com>
2026-02-03 14:51:12 +0530

    [Varsha] Added : UC1 : Vessel model with attributes and constructors

---
+++ src/Vessel.java
@@ -0,0 +1,52 @@
+public class Vessel {
+
+    private String vesselId;
+    private String vesselName;
+    private double averageSpeed;
+    private String vesselType;
+
+    // Default constructor
+    public Vessel() {
+    }
+
+    // Parameterized constructor
+    public Vessel(String vesselId, String vesselName, double averageSpeed, String vesselType) {
+        this.vesselId = vesselId;
+        this.vesselName = vesselName;
+        this.averageSpeed = averageSpeed;
+        this.vesselType = vesselType;
+    }
+
+    // Getters and Setters
+    public String getVesselId() {
+        return vesselId;
+    }
+
+    public void setVesselId(String vesselId) {
+        this.vesselId = vesselId;
+    }
+
+    public String getVesselName() {
+        return vesselName;
+    }
+
+    public void setVesselName(String vesselName) {
+        this.vesselName = vesselName;
+    }
+
+    public double getAverageSpeed() {
+        return averageSpeed;
+    }
+
+    public void setAverageSpeed(double averageSpeed) {
+        this.averageSpeed = averageSpeed;
+    }
+
+    public String getVesselType() {
+        return vesselType;
+    }
+
+    public void setVesselType(String vesselType) {
+        this.vesselType = vesselType;
+    }
+}
